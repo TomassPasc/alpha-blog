008fc4ae68b56b235c945eeb09f0c1c90e5fbcfa
ThomasPascual <thoma1@free.fr>
2020-07-03 11:01:36 +0200

    ajout des messages d'erreur et de succes pour la création d'un article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,17 @@ class ArticlesController < ApplicationController
     end
 
     def new
-    
+      @article = Article.new #initie un article pour que lorsque l'on affiche une page pour la première fois il y ait un objet
     end
 
     def create
-      #render plain: params[:article] #afficher sur le navigateur la création d'un article
-      @article = Article.new(params.require(:article).permit(:title, :description)) #permit est une sécurité paramètre fort pour les formulaires
-      #render plain: @article.inspect
-      @article.save #si on s'arrete là l'article est sauvegardé mais on reste sur la page
-      #redirect_to article_path(@article) #on redirige vers la page de l'article / article_path(@article) : extrait l'id de article object
-      redirect_to @article #meme action que precedemmet avec un shortcut
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        flash[:notice] = "L'article a bien été créée"
+        redirect_to @article
+      else
+        render 'new'
+      end
     end
 
 end
