cf154d07c327670b492d888ef15f7d19d97836e5
ThomasPascual <thoma1@free.fr>
2020-07-07 16:13:11 +0200

    pagination with the gems will_paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+    
     end
     
     def new
@@ -17,7 +22,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "your account information was succefully update"
-            redirect_to articles_path
+            redirect_to @user
         else
             render "edit"
         end
